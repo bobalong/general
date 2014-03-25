1aa74b7040aaed483dc483e5ed005e24af86bf57
emnes <mazshar@hotmail.co.uk>
2014-03-25 22:34:55 +0000

    Update gps.ino
    finished displayInfo() method to include date,time and speed.

--- gps/gps.ino
+++ gps/gps.ino
@@ -34,4 +34,13 @@ void displayInfo()
   Serial.println(gps.location.lat());
   Serial.print("LONG=");
   Serial.println(gps.location.lng());
+  Serial.print("DATE=");
+  Serial.println(gps.date.day() + "/" + gps.date.month() + "/" + gps.date.year());
+  Serial.print("TIME=");
+  Serial.println(gps.time.hour() + ":" gps.time.minute());
+  // delete as appropiate
+  Serial.println(gps.speed.knots()); // Speed in knots
+  Serial.println(gps.speed.mph()); // Speed in miles per hour
+  Serial.println(gps.speed.mps()); // Speed in meters per second
+  Serial.println(gps.speed.kmph()); // Speed in kilometers per hour
 }
